24c1d871e4f84b60a591b1b1ef3d9bd273043e97
jinkalavishnuvardhan <vishnusravan95@gmail.com>
2026-04-28 15:26:15 +0530

    connect railway mysql

--- Backend/index.js
+++ Backend/index.js
@@ -114,7 +114,7 @@ const bcrypt = require('bcrypt');
 // Set up the server
 const port = process.env.PORT || 5000; // Default to port 5000 if PORT is undefined
 app.get("/", (req, res) => {
-  res.send("Server + DB working 🚀");
+  res.send("App is working 🚀");
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
